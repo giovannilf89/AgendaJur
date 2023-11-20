1d308831c6d3cd7493e3612e4e2e221ddd4c3bff
Danilo Deverso <deverso@gmail.com>
2023-11-20 20:26:35 -0300

    file form

--- frontend/src/Processo/CriarProcesso.js
+++ frontend/src/Processo/CriarProcesso.js
@@ -18,84 +18,81 @@ export default function Processos() {
 
   const navigation = useNavigate();
 
-  useEffect(
-    () => {
-      const iToken = localStorage.getItem("@tklogin2023");
-      const token = JSON.parse(iToken);
+  const iToken = localStorage.getItem("@tklogin2023");
+  const token = JSON.parse(iToken);
 
-      async function loadCategorias() {
-        const resposta = await apiLocal.get("/ListarCategoria", {
-          headers: {
-            // eslint-disable-next-line no-useless-concat
-            Authorization: "Bearer " + `${token}`,
-          },
-        });
+  async function loadCategorias() {
+    const resposta = await apiLocal.get("/ListarCategoria", {
+      headers: {
+        // eslint-disable-next-line no-useless-concat
+        Authorization: "Bearer " + `${token}`,
+      },
+    });
 
-        setCategorias(resposta.data);
-      }
-      async function loadAdvogados() {
-        const resposta = await apiLocal.get("/ListarAdv", {
-          headers: {
-            // eslint-disable-next-line no-useless-concat
-            Authorization: "Bearer " + `${token}`,
-          },
-        });
-        setAdvogados(resposta.data);
-      }
-      async function loadClientes() {
-        const resposta = await apiLocal.get("/ListarCliente", {
-          headers: {
-            // eslint-disable-next-line no-useless-concat
-            Authorization: "Bearer " + `${token}`,
-          },
-        });
-        setClientes(resposta.data);
-      }
-      loadCategorias();
-      loadAdvogados();
-      loadClientes();
-    },
-    [],
-    [],
-    []
-  );
+    setCategorias(resposta.data);
+  }
+  async function loadAdvogados() {
+    const resposta = await apiLocal.get("/ListarAdv", {
+      headers: {
+        // eslint-disable-next-line no-useless-concat
+        Authorization: "Bearer " + `${token}`,
+      },
+    });
+    setAdvogados(resposta.data);
+  }
+  async function loadClientes() {
+    const resposta = await apiLocal.get("/ListarCliente", {
+      headers: {
+        // eslint-disable-next-line no-useless-concat
+        Authorization: "Bearer " + `${token}`,
+      },
+    });
+    setClientes(resposta.data);
+  }
+
+  useEffect (() => {
+    loadCategorias();
+    loadAdvogados();
+    loadClientes();
+  }, [])
+  
 
   function handleDocument(e) {
+    console.log(e.target.files)
     if (!e.target.files) {
       return;
     }
-    const document = e.target.files[0];
-    if (document.type === "document/pdf") {
-      setDocument(document);
+    if (e.target.files[0].type !== "application/pdf") {
+      alert("Só PDF, corno!")
+      return;
     }
+
+    const document = e.target.files[0];
+    setDocument(document);
   }
 
   async function handleCadastrar(e) {
-    console.log(e.target)
+    e.preventDefault();
     try {
-      // e.preventDefault();
-      const categoriaId = idCategoria;
-      const advogadoId = idAdvogado;
-      const clienteId = idCliente;
-      const data = new FormData();
+      const bebezao = new FormData();
 
-      data.append("numero", numero);
-      data.append("notas", notas);
-      data.append("categoriaId", categoriaId);
-      data.append("advogadoId", advogadoId);
-      data.append("clienteId", clienteId);
-      data.append("banner", e.target.files[0], e.target.files[0].name);
+      bebezao.append("numero", numero);
+      bebezao.append("notas", notas);
+      bebezao.append("categoriaId", idCategoria);
+      bebezao.append("advogadoId", idAdvogado);
+      bebezao.append("clienteId", idCliente);
+      bebezao.append("file", document, document.name);
 
       const iToken = localStorage.getItem("@tklogin2023");
       const token = JSON.parse(iToken);
       
-      const resposta = await apiLocal.post("/CriarProcesso", data, {
+      const resposta = await apiLocal.post("/CriarProcesso", bebezao, {
         headers: {
           // eslint-disable-next-line no-useless-concat
-          Authorization: "Bearer " + `${token}`,
+          Authorization: "Bearer " + token,
         },
       });
-      toast.success(resposta.data);
+      toast.success(resposta.bebezao);
     } catch (err) {
       console.log(err);
     }
@@ -114,9 +111,9 @@ export default function Processos() {
             value={idCategoria}
             onChange={(e) => setIdCategoria(e.target.value)}
           >
-            <option>Selecione...</option>
-            {categorias.map((item) => {
-              return <option value={item.id}>{item.nome}</option>;
+            <option value="1">Selecione...</option>
+            {categorias.map((item, idx) => {
+              return <option key={idx} value={item.id_cat}>{item.nome}</option>;
             })}
           </select>
           <br />
@@ -124,9 +121,9 @@ export default function Processos() {
             value={idAdvogado}
             onChange={(e) => setIdAdvogado(e.target.value)}
           >
-            <option>Selecione...</option>
-            {advogados.map((item) => {
-              return <option value={item.id}>{item.nome}</option>;
+            <option value="1">Selecione...</option>
+            {advogados.map((item, idx) => {
+              return <option key={idx} value={item.id_adv}>{item.nome}</option>;
             })}
           </select>
           <br />
@@ -134,9 +131,9 @@ export default function Processos() {
             value={idCliente}
             onChange={(e) => setIdCliente(e.target.value)}
           >
-            <option>Selecione...</option>
-            {clientes.map((item) => {
-              return <option value={item.id}>{item.nome}</option>;
+            <option value="1">Selecione...</option>
+            {clientes.map((item, idx) => {
+              return <option key={idx} value={item.id_cli}>{item.nome}</option>;
             })}
           </select>
           <br />
